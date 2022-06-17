97b38733fd69e8c860f4ae1542173287c389f849
Abhishek Singh <91729443+abhi2718@users.noreply.github.com>
2022-06-17 09:33:48 +0530

    Update list.dart

--- list.dart
+++ list.dart
@@ -20,3 +20,26 @@ List list=[1,2,3,4];
  // 4. removing unwanted item from list -> list.removeWhere((item)=>item["id"]!=="34")
 // 5. reversing a list -> List.reversed
 // 6. is list empty -> List.isEmpty->bool
+// 7. contains
+List list=[1,2,3,4];
+  var isContain=list.contains(2);
+  print(isContain);
+// output - > true
+
+// 8. finding index where test pass 
+List list=[1,2,3,4];
+  int index=list.indexWhere((element) => element==2 );
+  print(index);
+// output -> 1
+
+// 9. removing item at given index 
+List list=[1,2,3,4];
+  int index=list.indexWhere((element) => element==2 );
+  print(index);
+  list.removeAt(index);
+  print(list);
+
+// 10 . any element of list pass test
+ List list=[1,2,3];
+  var ans=list.any((element) => element==2 );
+  print(ans);
